e42a3d8659b9c15494075f29b2e959c4160df696
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2025-01-30 17:10:04 -0300

    test commit

--- src/TestInvertPlugin_.java
+++ src/TestInvertPlugin_.java
@@ -3,10 +3,13 @@ import ij.ImagePlus;
 import ij.plugin.PlugIn;
 
 public class TestInvertPlugin_ implements PlugIn {
-public void run(String arg) {
-	ImagePlus imagem = IJ.getImage();
-	IJ.run(imagem,"Invert", "");
-	IJ.wait(2500);
-	IJ.run(imagem, "Invert", "");
+	public void run(String arg) {
+		ImagePlus imagem = IJ.getImage();
+		for (int i = 0; i < 10; i++) {
+			IJ.run(imagem,"Invert", "");
+			IJ.wait(1000);
+			IJ.run(imagem, "Invert", "");
+			IJ.wait(1000);
+		}
 	}
 }
